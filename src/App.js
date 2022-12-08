69543f1c76fc41774d1abb0a6101b7258803a827
antoniojr100 <95947435+antoniojr100@users.noreply.github.com>
2022-12-07 22:12:17 -0500

    checking merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from "./Footer.js"
@@ -16,21 +15,12 @@ function App() {
     // In Welcome.js add welcome message and generate quote if the user has not selected a movie
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App wrapper">
+      <Header />
+      
+      <Main />
+
+      <Footer />
     </div>
   );
 }
